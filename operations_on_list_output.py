93992cb385ef7f141e07bc18a91f33b2a7c3166d
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-07-23 12:15:49 +0530

    Create operations_on_list_output.py

---
+++ operations_on_list_output.py
@@ -0,0 +1,22 @@
+append(6): [1, 2, 3, 4, 5, 6]
+extend([7, 8, 9]): [1, 2, 3, 4, 5, 6, 7, 8, 9]
+insert(3, 10): [1, 2, 3, 10, 4, 5, 6, 7, 8, 9]
+remove(10): [1, 2, 3, 4, 5, 6, 7, 8, 9]
+pop(): 9 ; list: [1, 2, 3, 4, 5, 6, 7, 8]
+pop(3): 4 ; list: [1, 2, 3, 5, 6, 7, 8]
+index(4): 3
+count(3): 1
+sort(): [1, 2, 3, 5, 6, 7, 8]
+sort(reverse=True): [8, 7, 6, 5, 3, 2, 1]
+reverse(): [1, 2, 3, 5, 6, 7, 8]
+copy(): [1, 2, 3, 5, 6, 7, 8]
+clear(): []
+sliced_list[1:4]: [2, 3, 4]
+list comprehension [x**2 for x in lst]: [1, 4, 9, 16, 25]
+3 in lst: True
+len(lst): 5
+max(lst): 5
+min(lst): 1
+sum(lst): 15
+list_of_lists: [[1, 2], [3, 4], [5, 6]]
+flattened_list: [1, 2, 3, 4, 5, 6]
